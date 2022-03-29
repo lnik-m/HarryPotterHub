12d38eda779e3e30a56d06ff7566454bcc84d5fe
lnik <marialnikova@gmail.com>
2022-03-29 17:43:32 +0300

    Header in Layout

--- hphub/Layout/Header/Header.tsx
+++ hphub/Layout/Header/Header.tsx
@@ -1,12 +1,19 @@
 import { HeaderProps } from './Header.props';
 import styles from './Header.module.scss';
 import cn from 'classnames';
+import HPLogo from './HPlogo.svg';
+import MenuLogo from './Menu.svg';
+import Link from 'next/link';
 
 export const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
 
 	return(
 		<div className={cn(className, styles.header)}>
-			Header
+			<Link href='/'>
+				<HPLogo />
+			</Link>
+
+			<MenuLogo className={styles.menu} />
 		</div>
 	);
 };
