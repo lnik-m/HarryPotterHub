6069df269754006caaa089669e1afe303307ecb1
lnik <marialnikova@gmail.com>
2022-03-29 18:11:53 +0300

    Sidebar draft

--- hphub/Layout/Sidebar/Sidebar.tsx
+++ hphub/Layout/Sidebar/Sidebar.tsx
@@ -1,12 +1,20 @@
 import { SidebarProps } from './Sidebar.props';
 import styles from './Sidebar.module.scss';
 import cn from 'classnames';
+import ArrowIcon from './Arrow.svg';
 
 export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
+	const Houses = ['Griffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw', 'Other'];
 
 	return(
 		<div className={cn(className, styles.sidebar)}>
-			Sidebar
+			{Houses.map(h => 
+			<div className={styles.sidebarItem}>
+				{h}
+				<ArrowIcon className={cn({
+					[styles.openedArrow]: false }
+				)}/>
+			</div>)}
 		</div>
 	);
 };
